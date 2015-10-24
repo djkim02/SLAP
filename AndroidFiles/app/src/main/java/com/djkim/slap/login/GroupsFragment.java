3c0d41f765a8c486c1fb52531eee48574eadd15e
Dong Joon Kim <dongjoonkim@wifi-131-179-47-100.host.ucla.edu>
2015-10-24 11:34:42 -0700

    First Commit

---
+++ AndroidFiles/app/src/main/java/com/djkim/slap/login/GroupsFragment.java
@@ -0,0 +1,20 @@
+package com.djkim.slap.login;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.djkim.slap.R;
+
+/**
+ * Created by dongjoonkim on 10/8/15.
+ */
+public class GroupsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.groups_fragment_layout, container, false);
+    }
+}
---
+++ AndroidFiles/app/src/main/java/com/djkim/slap/login/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.djkim.slap.login;
+
+/**
+ * Created by dongjoonkim on 10/8/15.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.djkim.slap.R;
+import com.viewpagerindicator.CirclePageIndicator;
+import com.viewpagerindicator.TitlePageIndicator;
+
+public class LoginActivity extends FragmentActivity {
+    //The number of pictures / pages to show
+    private static final int NUM_PAGES = 4;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new LoginPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        CirclePageIndicator circlePageIndicator = (CirclePageIndicator) findViewById(R.id.indicator);
+        circlePageIndicator.setViewPager(mPager);
+    }
+
+    private class LoginPagerAdapter extends FragmentPagerAdapter {
+        public LoginPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new SlapLoginFragment();
+                case 1:
+                    return new GroupsFragment();
+                case 2:
+                    return new HackerFragment();
+                case 3:
+                    return new AthleteFragment();
+                default:
+                    return new SlapLoginFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+}
