718526d6c8a04f7f2119503e78a22853f2fd7212
Dong Joon Kim <dongjoonkim@wifi-131-179-47-100.host.ucla.edu>
2015-10-24 13:29:50 -0700

    Pushed login branch

--- AndroidFiles/app/src/main/java/com/djkim/slap/login/LoginActivity.java
+++ AndroidFiles/app/src/main/java/com/djkim/slap/login/LoginActivity.java
@@ -4,6 +4,8 @@ package com.djkim.slap.login;
  * Created by dongjoonkim on 10/8/15.
  */
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -11,8 +13,14 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.widget.Toast;
 
 import com.djkim.slap.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.viewpagerindicator.CirclePageIndicator;
 import com.facebook.FacebookSdk;
 
@@ -21,13 +29,17 @@ public class LoginActivity extends FragmentActivity {
     private static final int NUM_PAGES = 4;
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private Activity currentActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
-
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.login_activity);
+        currentActivity = this;
 
         // Instantiate a ViewPager and a PagerAdapter.
         mPager = (ViewPager) findViewById(R.id.pager);
@@ -36,6 +48,33 @@ public class LoginActivity extends FragmentActivity {
         mPager.setPageTransformer(true, new ZoomOutPageTransformer());
         CirclePageIndicator circlePageIndicator = (CirclePageIndicator) findViewById(R.id.indicator);
         circlePageIndicator.setViewPager(mPager);
+
+        // setup login button
+        loginButton = (LoginButton) findViewById(R.id.login_with_facebook);
+        loginButton.setReadPermissions("user_friends");
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(currentActivity, "Login Successful!", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private class LoginPagerAdapter extends FragmentPagerAdapter {
