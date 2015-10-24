bdc90edb45e6b4aa6428b5ebc8a6cf6939cf2b3e
Kyle Nguyen <kylemn@ucla.edu>
2015-10-24 12:57:16 -0700

    Parse integracion

---
+++ AndroidFiles/app/src/main/java/com/djkim/slap/login/ParseApplication.java
@@ -0,0 +1,28 @@
+package com.djkim.slap.login;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by kylemn on 10/24/15.
+ */
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "6EZmymlOoCjIFnPPnJ13XcpeyyoXNIVoXTq2RwMo", "rNsM6txwCaBsM1SU5iw64l2GUHCxQhXmhYMcePHK");
+
+        //TODO: remove l8r
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+    }
+}
