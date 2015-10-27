db4115db7b85a1d214308adfa0c91719d5a8078a
Dong Joon Kim <dongjoonkim@Dong-Joons-MacBook-Pro.local>
2015-10-27 00:44:09 -0700

    Added icons and created Skill class

--- AndroidFiles/app/src/main/java/com/djkim/slap/profile/CreateProfileHackerFragment.java
+++ AndroidFiles/app/src/main/java/com/djkim/slap/profile/CreateProfileHackerFragment.java
@@ -5,13 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.djkim.slap.R;
+import com.djkim.slap.models.Skill;
+
+import java.util.ArrayList;
 
 /**
  * Created by dongjoonkim on 10/25/15.
  */
 public class CreateProfileHackerFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
