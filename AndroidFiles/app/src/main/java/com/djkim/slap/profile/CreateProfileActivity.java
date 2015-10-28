4277efcb5e3bc15d8f58748ee1eb6c31ba3613d6
Dong Joon Kim <dongjoonkim@Dong-Joons-MacBook-Pro.local>
2015-10-28 00:57:17 -0700

    Fixed basketball icon

--- AndroidFiles/app/src/main/java/com/djkim/slap/profile/CreateProfileActivity.java
+++ AndroidFiles/app/src/main/java/com/djkim/slap/profile/CreateProfileActivity.java
@@ -66,6 +66,7 @@ public class CreateProfileActivity extends FragmentActivity {
                     user.set_hacker_skills(hackerFragment.updateHackerSkills());
                     createProfileTitle.setText("Tell us about yourself");
                 } else if (currentItem == 2) {
+                    user.set_athlete_skills(athleteFragment.updateAthleteSkills());
                     createProfileTitle.setText("Programming Skills");
                 } else if (currentItem == 3) {
                     nextButton.setText("Next");
@@ -86,6 +87,7 @@ public class CreateProfileActivity extends FragmentActivity {
                     user.set_hacker_skills(hackerFragment.updateHackerSkills());
                     createProfileTitle.setText("Athlete Skills");
                 } else if (currentItem == 2) {
+                    user.set_athlete_skills(athleteFragment.updateAthleteSkills());
                     createProfileTitle.setText("Congratulations!");
                     nextButton.setText("Done");
                 }
