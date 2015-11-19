c7ce35f9f60335cdf08cfade9e2d9613261668b4
yj94choi <yjchoi0122@gmail.com>
2015-11-18 21:14:38 -0800

    Show owner's profile pic in GroupListFragment.

--- AndroidFiles/app/src/main/java/com/djkim/slap/home/GroupListFragment.java
+++ AndroidFiles/app/src/main/java/com/djkim/slap/home/GroupListFragment.java
@@ -4,6 +4,8 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
@@ -26,6 +28,7 @@ import com.djkim.slap.models.GroupCallback;
 import com.djkim.slap.models.GroupsCallback;
 import com.djkim.slap.models.User;
 import com.djkim.slap.models.Utils;
+import com.facebook.login.widget.ProfilePictureView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -74,20 +77,25 @@ public class GroupListFragment extends Fragment {
 
     private class GroupHolder extends RecyclerView.ViewHolder {
         private Group mGroup;
-        private ImageView mThumbnailImageView;
+        private ProfilePictureView mThumbnailImageView;
         private TextView mTitleTextView;
         private TextView mSubheadTextView;
         private TextView mSkillsTextView;
         private TextView mTagsTextView;
         private Button mDetailsButton;
         private Button mRemainingSlotsButton;
+        private ImageView mSubtractedCircle;
 
         // TODO(victorkwan): Declare and assign the relevant fields.
         public GroupHolder(View itemView) {
             super(itemView);
 
+            mSubtractedCircle = (ImageView) itemView.findViewById(R.id.group_list_item_subtracted_circle);
+            int color = Color.parseColor("#009688");
+            mSubtractedCircle.setColorFilter(color);
+
             mThumbnailImageView =
-                    (ImageView) itemView.findViewById(R.id.group_list_item_thumbnail_image_view);
+                    (ProfilePictureView) itemView.findViewById(R.id.group_list_item_thumbnail_image_view);
 
             mTitleTextView = (TextView) itemView.findViewById(R.id.group_list_item_title_text_view);
             mTitleTextView.setText("Friday Night Soccer!");
@@ -167,6 +175,7 @@ public class GroupListFragment extends Fragment {
             String text = group.get_type() + " Group";
             User owner = group.get_owner();
             if (owner != null) {
+                mThumbnailImageView.setProfileId(owner.get_facebook_profile_id());
                 text += " created by " + owner.get_name();
             }
             mSubheadTextView.setText(text);
