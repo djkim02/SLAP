3562457bfa43e2850e97e32667932bf57706c77d
Calvin <calvinliu411@gmail.com>
2015-11-26 00:05:47 -0800

    Generated new GroupTest.java

---
+++ AndroidFiles/app/src/test/java/com/djkim/slap/models/GroupTest.java
@@ -0,0 +1,28 @@
+package com.djkim.slap.models;
+
+import android.test.InstrumentationTestCase;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserTest {
+
+}
