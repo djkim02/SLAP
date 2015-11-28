e8ca17968da5c3a3aabb9e2d403faf734154b53e
yj94choi <yjchoi0122@gmail.com>
2015-11-27 16:02:57 -0800

    Fix bug when group skills is empty

--- cloud/main.js
+++ cloud/main.js
@@ -4,13 +4,13 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
-  
+   
 // Parse.Cloud.define("search", function(request, response) {
 //  var query = new Parse.Query("Group");
 //  query.contains("name", request.params.searchKey);
-  
+   
 // });
-  
+   
 // TODO(yj94choi): custom tags
 Parse.Cloud.define("match", function(request, response) {
     var memberQuery = request.user.relation("memberOf").query();
@@ -43,11 +43,13 @@ Parse.Cloud.define("match", function(request, response) {
                             continue;
                         }
                         var skills = results[i].get("skills");
-                        var skillArray = skills.split(", ");
                         var satisfies = true;
-                        for (var j = 0; j < skillArray.length; j++) {
-                            if (!(skillArray[j] in userSkillSet)) {
-                                satisfies = false;
+                        if (skills !== "") {
+                            var skillArray = skills.split(", ");
+                            for (var j = 0; j < skillArray.length; j++) {
+                                if (!(skillArray[j] in userSkillSet)) {
+                                    satisfies = false;
+                                }
                             }
                         }
                         if (satisfies) {
@@ -65,8 +67,8 @@ Parse.Cloud.define("match", function(request, response) {
             response.error("memberOf group fetch failed");
         }
     });
-
-
+ 
+ 
     // query.notEqualTo("members", request.user);
     // query.equalTo("type", request.params.type);
     // query.descending("createdAt");
@@ -105,7 +107,7 @@ Parse.Cloud.define("match", function(request, response) {
     //     }
     // });
 });
-  
+   
 Parse.Cloud.define("matchGroupName", function(request, response) {
     var query = new Parse.Query("Group");
     query.equalTo("name", request.params.name);
@@ -120,57 +122,57 @@ Parse.Cloud.define("matchGroupName", function(request, response) {
         }
     });
 });
-  
-  
+   
+   
 var _ = require("underscore");
 Parse.Cloud.beforeSave("Group", function(request, response) {
     var group = request.object;
-  
+   
     var toLowerCase = function(w) { return w.toLowerCase(); };
-  
+   
     var keywords = group.get("name").split(' ');
     keywords = _.map(keywords, toLowerCase);
     var stopWords = ["the", "in", "and", "to", "but", "for", "or", "yet", "so"];
-     
+      
     Array.prototype.contains = function ( needle ) {
     for (i in this) {
        if (this[i] == needle) return true;
     }
     return false;
     }
-     
+      
     keywords = keywords.filter(function (w) {
     return w.match(/[a-zA-Z0-9]+/) && !stopWords.contains(w);
     });
-  
+   
     var hashtags = keywords;
-	hashtags = hashtags.filter(function (w) {
+    hashtags = hashtags.filter(function (w) {
     return w.indexOf('#') > -1;
     });
-  
+   
     group.set("keywords", keywords);
     group.set("hashtags", hashtags);
     response.success();
 });
- 
+  
 Parse.Cloud.define("partialStringSearch", function(request, response) {
     var toLowerCase = function(w) { return w.toLowerCase(); };
-  
+   
     var keywords = request.params.name.split(' ');
     keywords = _.map(keywords, toLowerCase);
     var stopWords = ["the", "in", "and", "to", "but", "for", "or", "yet", "so"];
-     
+      
     Array.prototype.contains = function ( needle ) {
     for (i in this) {
        if (this[i] == needle) return true;
     }
     return false;
     }
-     
+      
     keywords = keywords.filter(function (w) {
     return w.match(/[a-zA-Z0-9]+/) && !stopWords.contains(w);
     });
-     
+      
     var query = new Parse.Query("Group");
     query.containsAll("keywords", keywords);
     query.include("owner, members");
@@ -183,9 +185,9 @@ Parse.Cloud.define("partialStringSearch", function(request, response) {
             response.error("group fetch failed");
         }
     });
-     
-     
-  
+      
+      
+   
      //TO DO: search hashtags
     // var hashtags = group.get("name").match(/[#][a-zA-Z0-9]+/);
     // hashtags = _.map(hashtags, toLowerCase);
